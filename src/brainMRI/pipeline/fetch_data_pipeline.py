4f2d0e98ac5a583a31b090eb7ff67ff4f88bd381
rezjsh <149146755+rezjsh@users.noreply.github.com>
2024-05-16 15:13:35 +0330

    add config as parameter to pipeline

--- src/brainMRI/pipeline/fetch_data_pipeline.py
+++ src/brainMRI/pipeline/fetch_data_pipeline.py
@@ -1,19 +1,23 @@
 from brainMRI.config.configuration import ConfigHandler
 from brainMRI.logging import logger
 
-config = ConfigHandler()
+
 
 class FetchDataPipeline:
+    def __init__(self, config) -> None:
+            self.config = config
+
     def main(self):
-        fetch_data_config = config.get_fetch_data_config()
+        fetch_data_config = self.config.get_fetch_data_config()
         fetch_data_config.download_file()
         fetch_data_config.unzip_file()
 
 if __name__ == '__main__':
     try:
+        config = ConfigHandler()
         stage_name = 'Fetch Data stage'
         logger.info(f">>>>>> stage {stage_name} started <<<<<<")  # Log the start of the pipeline stage
-        pipeline = FetchDataPipeline()
+        pipeline = FetchDataPipeline(config)
         pipeline.main()
         logger.info(f">>>>>> stage {stage_name} completed <<<<<<\n\nx==========x")  # Log the completion of the pipeline stage
 
